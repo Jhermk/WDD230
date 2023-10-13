5e9edc4aa2988816d692a06068eb8070249acf3d
Jhermk <lakoffmanie2018@gmail.com>
2023-10-13 16:34:45 +0000

    update

---
+++ scripts/links.js
@@ -0,0 +1,35 @@
+const baseURL = "https://jhermk.github.io/wdd230/";
+const linksURL = "/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data.weeks);
+}
+
+function displayLinks(weeks) {
+    const learningActivitiesList = document.getElementById('learning-activities-list');
+
+    weeks.forEach(week => {
+        const weekItem = document.createElement('li');
+        const weekLinks = week.links.map(link => {
+            const linkElement = document.createElement('a');
+            linkElement.href = `${baseURL}${link.url}`;
+            linkElement.textContent = link.title;
+            return linkElement;
+        });
+
+        weekItem.appendChild(document.createTextNode(`${week.week}: `));
+
+        for (let i = 0; i < weekLinks.length; i++) {
+            weekItem.appendChild(weekLinks[i]);
+            if (i < weekLinks.length - 1) {
+                weekItem.appendChild(document.createTextNode(' | '));
+            }
+        }
+
+        learningActivitiesList.appendChild(weekItem);
+    });
+}
+
+getLinks();
---
+++ scripts/weather.js
@@ -0,0 +1,43 @@
+// Select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#weather-description');
+
+const apiKey = '2c7980475c08ffbafe5117afe00a84ee';
+const lat = 5.36;
+const lon = 4.00;
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); 
+            displayResults(data); 
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    const temperature = data.main.temp; // Temperature in Kelvin
+    const iconCode = data.weather[0].icon;
+    const description = data.weather[0].description;
+
+    // Convert temperature to Fahrenheit
+    const fahrenheit = (temperature - 273.15) * 9 / 5 + 32;
+
+    currentTemp.innerHTML = `${Math.round(fahrenheit)}&deg;F`;
+    const iconSrc = `https://openweathermap.org/img/w/${iconCode}.png`;
+    weatherIcon.setAttribute('src', iconSrc);
+    weatherIcon.setAttribute('alt', description);
+    captionDesc.textContent = description; // Display weather description
+}
+
